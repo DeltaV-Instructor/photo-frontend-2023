d245a4d5f93c641a277e3e9d3ddabeb7efd54780
tektechnologies <info@tektechnologies.com>
2023-10-25 11:26:43 -0500

    add file scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Button, Carousel, Container, Form } from 'react-bootstrap';
+
+
+
+
+class App extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
